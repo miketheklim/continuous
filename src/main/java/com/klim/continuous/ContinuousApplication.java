eaaf50b71d65cb91bb0c35bdd005e07412a517bd
miketheklim <miketheklim@icloud.com>
2019-03-21 09:48:14 +0100

    Initial commit

---
+++ src/main/java/com/klim/continuous/ContinuousApplication.java
@@ -0,0 +1,13 @@
+package com.klim.continuous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klim/continuous/ContinuousApplicationTests.java
@@ -0,0 +1,16 @@
+package com.klim.continuous;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContinuousApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
